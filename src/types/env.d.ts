680708974b2d724ce839ca925080032490f30b02
diantanjung <dianmulyanat@gmail.com>
2024-06-08 14:10:46 +0700

    Feat/project structure (#2)
    * move env type to src/types
    
    * fix: auto complete Environment type

---
+++ src/types/env.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  interface Environment {
+    DATABASE_URL?: string;
+    SALT_ROUND?: string;
+  }
+  namespace NodeJS {
+    interface ProcessEnv extends Environment {}
+  }
+}
+
+
+export {};
